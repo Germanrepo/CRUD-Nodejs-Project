63189c21b95fcf25518d00f71d8bebce95f3369d
German Centurion <gecenturion99@gmail.com>
2023-09-09 23:38:53 -0300

    change env vars

--- dataBase/connectionConfig.js
+++ dataBase/connectionConfig.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
     database: {
-        host: process.env.HOST,
-        user: process.env.USER,
-        database: process.env.DATABASE,
-        password: process.env.PASSWORD,
-        port: process.env.PORTDB
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT
     }
 }
